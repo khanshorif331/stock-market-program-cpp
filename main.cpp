cdf44e265d07d95ec0dd7393f8d2387f9e271ad2
khanshorif331 <khanshorif331@gmail.com>
2022-10-09 22:07:31 +0600

    coded a simple programe to calculate amount of mney

--- main.cpp
+++ main.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
+#include <cmath>
+
 using namespace std;
+
+// it's a programme where calcuting how much we can earn if we keep 10000 bucks in the stock market and we are getting 1% interest every day.and at the end of 20 day let's calculaate...
+// a = amount of money we will have after 20days
+
+// p = principal amount we have
+// r = rate of interest
 
 int main()
 {
-    cout << "Hey,whats up?" << endl;
+    float a;
+    float p = 10000;
+    float r = 0.01;
+
+    for (int day = 1; day <= 20; day++)
+    {
+        a = p * pow(1 + r, day); // it's a formula
+        cout << day << "-----" << a << endl;
+    }
 }
